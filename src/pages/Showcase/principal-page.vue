62ffdce885d05a1d56121b3c7a351e168ab7eaa3
Diogo  Alberto <106744072+albertokff@users.noreply.github.com>
2024-07-16 23:43:20 -0300

    ✨feat: navbar prepared

--- src/pages/Showcase/principal-page.vue
+++ src/pages/Showcase/principal-page.vue
@@ -4,12 +4,21 @@
     style="height:100vh;width:100vw;"
   >    
     <div class="row">
-      <div class="col-md-6">
-        <a class="navbar-brand" href="#">VoolDesign</a>
-        <p>Baixe seu arquivo facilmente.</p>                                   
+      <div class="col-md-6 mt-10">
+        <div>
+          <i class="fa-brands fa-dropbox fa-2xl mr-5" style="color: #000000;font-size:50px;margin-left:30px;" />
+          <a class="navbar-brand" href="#">
+            <span style="font-weight: 900;font-size:30px;color:black;">
+              VoolDesign
+            </span>
+          </a>
+          <p class="text-black" style="margin-left:90px;">Baixe seu arquivo facilmente.</p>                                   
+        </div>
       </div>
-      <div class="col-md-6 d-flex justify-end">            
-        <button class="btn btn-warning text-black p-10" type="submit" style="border:3px solid black;">Começar</button>        
+      <div class="col-md-6" style="display:flex;justify-content: end;align-items: center;padding-right:70px;">            
+        <span class="mt-10 text-black">Planos</span>
+        <span class="mt-10 text-black ml-10"><i class="fa-solid fa-user" style="color: #000000;"></i>&nbsp;&nbsp;Login</span>
+        <button class="btn btn-warning text-black mt-10 ml-10" type="submit" style="border:3px solid black;height:40px;">Começar</button>        
       </div>
     </div>                 
   </div>
